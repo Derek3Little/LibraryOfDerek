220ba0279c550de92d78a9c263907a752b3fb460
Derek E Little <derek3little@gmail.com>
2023-01-03 12:37:28 -0500

    Initial commit, starting LibraryOfDerek using Sprint Boot Initializr.

---
+++ src/main/java/com/DLapp/LibraryOfDerek/LibraryOfDerekApplication.java
@@ -0,0 +1,13 @@
+package com.DLapp.LibraryOfDerek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryOfDerekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryOfDerekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DLapp/LibraryOfDerek/LibraryOfDerekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DLapp.LibraryOfDerek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryOfDerekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
