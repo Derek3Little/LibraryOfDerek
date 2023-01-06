66ecc5f848a02749e08c04b13730712cb55da27d
Derek E Little <derek3little@gmail.com>
2023-01-06 09:50:48 -0500

    Creating BookRepository interface.

---
+++ src/main/java/com/DLapp/LibraryOfDerek/Repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.DLapp.LibraryOfDerek.Repository;
+
+import com.DLapp.LibraryOfDerek.Entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> { // repository of Books, identified by Long bookId
+    
+}
