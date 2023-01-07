9d33a072793bdf0c0b949f9690158a7d49140655
Derek E Little <derek3little@gmail.com>
2023-01-07 14:27:02 -0500

    Creating BookController class.

---
+++ src/main/java/com/DLapp/LibraryOfDerek/Controller/BookController.java
@@ -0,0 +1,10 @@
+package com.DLapp.LibraryOfDerek.Controller;
+
+import com.DLapp.LibraryOfDerek.Service.BookService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+
+    private BookService bookService;
+}
