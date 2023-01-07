75b9bbd0f46fa9b7b36f30f7ef639e4e34175e40
Derek E Little <derek3little@gmail.com>
2023-01-07 14:30:28 -0500

    Adding findAllBooks method to BookController class.

--- src/main/java/com/DLapp/LibraryOfDerek/Controller/BookController.java
+++ src/main/java/com/DLapp/LibraryOfDerek/Controller/BookController.java
@@ -1,10 +1,22 @@
 package com.DLapp.LibraryOfDerek.Controller;
 
+import com.DLapp.LibraryOfDerek.Entity.Book;
 import com.DLapp.LibraryOfDerek.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Controller
 public class BookController {
 
+    @Autowired
     private BookService bookService;
+
+    public String findAllBooks(Model model) {
+        List<Book> books = bookService.findAllBooks();
+        model.addAttribute("books", books);
+        return "books";
+    }
 }
