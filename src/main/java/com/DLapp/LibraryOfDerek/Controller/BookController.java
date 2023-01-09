7d242c68534cb7247d585b71b349fa19c3a6ff25
Derek E Little <derek3little@gmail.com>
2023-01-09 11:18:18 -0500

    Creating updateBook function and mapping in BookController class.

--- src/main/java/com/DLapp/LibraryOfDerek/Controller/BookController.java
+++ src/main/java/com/DLapp/LibraryOfDerek/Controller/BookController.java
@@ -1,7 +1,10 @@
 package com.DLapp.LibraryOfDerek.Controller;
 
 import com.DLapp.LibraryOfDerek.Entity.Book;
+import com.DLapp.LibraryOfDerek.Service.AuthorService;
 import com.DLapp.LibraryOfDerek.Service.BookService;
+import com.DLapp.LibraryOfDerek.Service.CategoryService;
+import com.DLapp.LibraryOfDerek.Service.PublisherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +19,15 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private PublisherService publisherService;
+
+    @Autowired
+    private AuthorService authorService;
+
     @GetMapping("/books")
     public String findAllBooks(Model model) {
         List<Book> books = bookService.findAllBooks();
@@ -28,5 +40,21 @@ public class BookController {
         Book book = bookService.findBookById(id);
         model.addAttribute("book", book);
         return "list-book";
+    }
+
+    @GetMapping("remove-book/{id}")
+    public String deleteBook(@PathVariable Long id, Model model) {
+        bookService.deleteBook(id);
+        model.addAttribute("books", bookService.findAllBooks());
+        return "books"; // returning the book list reflecting deleted book
+    }
+
+    @GetMapping("/update-book/{id}")
+    public String updateBook(@PathVariable Long id, Model model) {
+        Book book = bookService.findBookById(id);
+        model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute("publishers", publisherService.getAllPublishers());
+        model.addAttribute("authors", authorService.getAllAuthors());
+        return "update-book";
     }
 }
