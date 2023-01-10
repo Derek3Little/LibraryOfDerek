848518cbf6229f4fd96a1b9e0bf2aa0d28ad1066
Derek E Little <derek3little@gmail.com>
2023-01-10 12:17:36 -0500

    Creating CategoryController class.

---
+++ src/main/java/com/DLapp/LibraryOfDerek/Controller/CategoryController.java
@@ -0,0 +1,21 @@
+package com.DLapp.LibraryOfDerek.Controller;
+
+import com.DLapp.LibraryOfDerek.Service.CategoryService;
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/categories")
+    public String findAllCategories(Model model) {
+        model.addAttribute("categories", categoryService);
+        return "categories";
+    }
+
+}
