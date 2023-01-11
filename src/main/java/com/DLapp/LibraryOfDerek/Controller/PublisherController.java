a93e51197f070aed7d0b725eec4614ce00ca3774
Derek E Little <derek3little@gmail.com>
2023-01-11 11:29:04 -0500

    Adding saveUpdatePublisher method to PublisherController class.

--- src/main/java/com/DLapp/LibraryOfDerek/Controller/PublisherController.java
+++ src/main/java/com/DLapp/LibraryOfDerek/Controller/PublisherController.java
@@ -1,11 +1,14 @@
 package com.DLapp.LibraryOfDerek.Controller;
 
+import com.DLapp.LibraryOfDerek.Entity.Publisher;
 import com.DLapp.LibraryOfDerek.Service.PublisherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PublisherController {
@@ -30,6 +33,19 @@ public class PublisherController {
     public String updatePublisher(@PathVariable Long id, Model model) {
         model.addAttribute("publisher", publisherService.findPublisherById(id));
         return "update-publisher";
+    }
+
+    @PostMapping("/update-publisher/{id}")
+    public String saveUpdatePublisher(@PathVariable Long id, Publisher publisher, BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            return "update-publisher";
+        }
+
+        publisherService.updatePublisher(publisher);
+        model.addAttribute("publishers", publisherService.getAllPublishers());
+
+        return "redirect:/publishers";
     }
 
 }
