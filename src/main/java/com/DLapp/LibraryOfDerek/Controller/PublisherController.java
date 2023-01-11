017bce865d1762f18543acfcdd9a99b3300bf75c
Derek E Little <derek3little@gmail.com>
2023-01-11 10:26:09 -0500

    Adding deletePublisher function to PublisherController class.

--- src/main/java/com/DLapp/LibraryOfDerek/Controller/PublisherController.java
+++ src/main/java/com/DLapp/LibraryOfDerek/Controller/PublisherController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PublisherController {
@@ -16,6 +17,13 @@ public class PublisherController {
     public String findAllPublishers(Model model) {
         model.addAttribute("publishers", publisherService.getAllPublishers());
         return "publishers";
+    }
+
+    @GetMapping("/remove-publisher/{id}")
+    public String deletePublisher(@PathVariable Long id, Model model) {
+        publisherService.deletePublisher(id);
+        model.addAttribute("publishers", publisherService.getAllPublishers());
+        return "redirect:/publishers"; // redirect to put user back on publishers page vs remove-publisher/{id} page
     }
 
 }
