f728322f623a78b1172b52581516f3c6494917a5
Derek E Little <derek3little@gmail.com>
2023-01-11 13:12:34 -0500

    Adding removeAuthor function to PublisherController class.

--- src/main/java/com/DLapp/LibraryOfDerek/Controller/AuthorController.java
+++ src/main/java/com/DLapp/LibraryOfDerek/Controller/AuthorController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AuthorController {
@@ -17,4 +18,12 @@ public class AuthorController {
         model.addAttribute("authors", authorService.getAllAuthors());
         return "authors";
     }
+
+    @GetMapping("/remove-author/{id}")
+    public String removeAuthor(@PathVariable Long id, Model model) {
+        authorService.deleteAuthor(id); // delete the author by id
+        model.addAttribute("authors", authorService.getAllAuthors()); // repopulate authors
+        return "redirect:/authors"; // redirect to get user off remove-author page
+    }
+    
 }
