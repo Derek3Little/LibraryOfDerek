2c3123d1f6ab9c07d3b22f5a4c02205eed52d440
Derek E Little <derek3little@gmail.com>
2023-01-11 09:56:08 -0500

    Adding findAllPublishers function to PublisherController class.

--- src/main/java/com/DLapp/LibraryOfDerek/Controller/PublisherController.java
+++ src/main/java/com/DLapp/LibraryOfDerek/Controller/PublisherController.java
@@ -3,11 +3,19 @@ package com.DLapp.LibraryOfDerek.Controller;
 import com.DLapp.LibraryOfDerek.Service.PublisherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PublisherController {
 
     @Autowired
     private PublisherService publisherService;
+
+    @GetMapping("/publishers")
+    public String findAllPublishers(Model model) {
+        model.addAttribute("publishers", publisherService.getAllPublishers());
+        return "publishers";
+    }
 
 }
