dec6831b721fe356206d3814397b6eb8d2a4cab6
Derek E Little <derek3little@gmail.com>
2023-01-11 12:12:43 -0500

    Adding findAllAuthors function to AuthorController class.

--- src/main/java/com/DLapp/LibraryOfDerek/Controller/AuthorController.java
+++ src/main/java/com/DLapp/LibraryOfDerek/Controller/AuthorController.java
@@ -3,11 +3,18 @@ package com.DLapp.LibraryOfDerek.Controller;
 import com.DLapp.LibraryOfDerek.Service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthorController {
 
     @Autowired
     private AuthorService authorService;
-    
+
+    @GetMapping("/authors")
+    public String findAllAuthors(Model model) {
+        model.addAttribute("authors", authorService.getAllAuthors());
+        return "authors";
+    }
 }
