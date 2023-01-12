9a2c090bf160a668896d448c8f0ab95b3b003d94
Derek E Little <derek3little@gmail.com>
2023-01-12 11:17:33 -0500

    Creating index.html for home page.

--- src/main/java/com/DLapp/LibraryOfDerek/Controller/IndexController.java
+++ src/main/java/com/DLapp/LibraryOfDerek/Controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
+    @GetMapping("/") // no path, homepage!
     public String listHome() {
         return "index";
     }
