bd06389884a1eafa2b34fdf3c2aa7cacfc898fd2
Derek E Little <derek3little@gmail.com>
2023-01-18 14:21:12 -0500

    Hard-coding 'real' starter books to enable testing/demo.

--- src/main/java/com/DLapp/LibraryOfDerek/LibraryOfDerekApplication.java
+++ src/main/java/com/DLapp/LibraryOfDerek/LibraryOfDerekApplication.java
@@ -22,28 +22,33 @@ public class LibraryOfDerekApplication {
 	public CommandLineRunner initialCreate(BookService bookService) { // adding initial books for testing
 		return(args) -> {
 
-			Book book1 = new Book("ABC", "Book name", "My first book");
-			Author author1 = new Author("Test name1", "Test description");
-			Category category1 = new Category("Business books");
-			Publisher publisher1 = new Publisher("First Publisher");
+			Book book1 = new Book("9780674040755", "The Ants", "Myrmecology Textbook");
+			Author author1 = new Author("Edward O. Wilson", "World's foremost myrmecologist!");
+			Category category1 = new Category("Biology");
+			Publisher publisher1 = new Publisher("Belknap Press: An Imprint of Harvard University Press; " +
+					"1st edition (March 28, 1990)");
 			book1.addAuthor(author1);
 			book1.addCategory(category1);
 			book1.addPublisher(publisher1);
 			bookService.createBook(book1);
 
-			Book book2 = new Book("ABC1", "Book name", "My second book");
-			Author author2 = new Author("Test name2", "Test description");
-			Category category2 = new Category("Science books");
-			Publisher publisher2 = new Publisher("Second Publisher");
+			Book book2 = new Book("1015494072",
+					"The Genealogical History of the Gallup Family in the United States",
+					"Maternal Genealogy");
+			Author author2 = new Author("John Douglass Gallup", "A not so distant cousin!");
+			Category category2 = new Category("Maternal Genealogy");
+			Publisher publisher2 = new Publisher("Legare Street Press (October 26, 2022)");
 			book2.addAuthor(author2);
 			book2.addCategory(category2);
 			book2.addPublisher(publisher2);
 			bookService.createBook(book2);
 
-			Book book3 = new Book("ABC21", "Book name", "My third book");
-			Author author3 = new Author("Test name3", "Test description");
-			Category category3 = new Category("Fiction books");
-			Publisher publisher3 = new Publisher("Third Publisher");
+			Book book3 = new Book("1611212626",
+					"Fighting for General Lee: Confederate General Rufus Barringer and the North Carolina Cavalry Brigade",
+					"Paternal Genealogy, signed by author!");
+			Author author3 = new Author("Sheridan Barringer", "A distant cousin!");
+			Category category3 = new Category("Paternal Genealogy");
+			Publisher publisher3 = new Publisher("Savas Beatie (March 10, 2016)");
 			book3.addAuthor(author3);
 			book3.addCategory(category3);
 			book3.addPublisher(publisher3);
