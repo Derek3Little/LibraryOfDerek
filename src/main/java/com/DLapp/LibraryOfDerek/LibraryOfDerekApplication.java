faa873d128b1bb3fa50337d4a1d7215f115e6f2d
Derek E Little <derek3little@gmail.com>
2023-02-10 08:00:29 -0500

    Cleaning up comments related to default books in LibraryOfDerekApplication.

--- src/main/java/com/DLapp/LibraryOfDerek/LibraryOfDerekApplication.java
+++ src/main/java/com/DLapp/LibraryOfDerek/LibraryOfDerekApplication.java
@@ -19,8 +19,10 @@ public class LibraryOfDerekApplication {
 	}
 
 	@Bean
-	public CommandLineRunner initialCreate(BookService bookService) { // adding initial books for testing
+	public CommandLineRunner initialCreate(BookService bookService) {
 		return(args) -> {
+
+			// adding initial books for testing
 
 			Book book1 = new Book("9780674040755", "The Ants", "Myrmecology Textbook");
 			Author author1 = new Author("Edward O. Wilson", "World's foremost myrmecologist!");
@@ -54,49 +56,6 @@ public class LibraryOfDerekApplication {
 			book3.addPublisher(publisher3);
 			bookService.createBook(book3);
 
-			/* more detailed sample books
-
-			Book book1 = new Book("9780674040755",
-					"The Ants (First Edition)",
-					"The distillation of a lifetime of research by the world’s leading myrmecologists.");
-			Author author1 = new Author("Edward O. Wilson",
-					 "The world's leading expert in his specialty of myrmecology");
-			Author author2 = new Author("Bert Holldobler",
-					"Sociobiologist and evolutionary biologist who studies evolution and social organization in ants.");
-			Category category1 = new Category("Biology");
-			Publisher publisher1 = new Publisher("Belknap Press: An Imprint of Harvard University Press; " +
-					"1st edition (March 28, 1990)");
-			book1.addAuthor(author1);
-			book1.addAuthor(author2);
-			book1.addCategory(category1);
-			book1.addPublisher(publisher1);
-			bookService.createBook(book1);
-
-			Book book2 = new Book("1015494072",
-					"The Genealogical History of the Gallup Family in the United States",
-					"A detailed survey of the Gallup Family Tree starting from 1630.");
-			Author author3 = new Author("John Douglass Gallup",
-					"A somewhat distant relative of none other than Derek himself!");
-			Category category2 = new Category("Genealogy");
-			Publisher publisher2 = new Publisher("Legare Street Press (October 26, 2022)");
-			book2.addAuthor(author3);
-			book2.addCategory(category2);
-			book2.addPublisher(publisher2);
-			bookService.createBook(book2);
-
-			Book book3 = new Book("1611212626",
-					"Fighting for General Lee: Confederate General Rufus Barringer and the " +
-							"North Carolina Cavalry Brigade",
-					"A Biography of Rufus Barringer, Confederate General");
-			Author author4 = new Author("Sheridan R. Barringer",
-					"Another somewhat distant relative of none other than Derek himself!");
-			Publisher publisher3 = new Publisher("Savas Beatie (March 10, 2016)");
-			book3.addAuthor(author3);
-			book3.addCategory(category2);
-			book3.addPublisher(publisher3);
-			bookService.createBook(book3);
-
-			 */
 		};
 	}
 }
